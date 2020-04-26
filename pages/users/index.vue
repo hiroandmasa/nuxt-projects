bd281447ae65296e87b07477c5c959d53c7b0ee5
池田雅裕 <m-ikeda@sunseer.co.jp>
2020-04-26 16:46:51 +0900

    vue routerのテスト

---
+++ pages/users/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>/users/index.vue</h1>
+    <hr>
+    <router-link to="/users/other">other</router-link>
+  </div>
+</template>
---
+++ pages/users/other.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>/users/other.vue</h1>
+    <hr>
+    <router-link to="/users/">users</router-link>
+  </div>
+</template>
